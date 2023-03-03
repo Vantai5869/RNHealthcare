44b06639e66c28e8581be1321dd9995bbc07a993
Vantai5869 <50700980+Vantai5869@users.noreply.github.com>
2023-03-03 14:50:42 +0700

    add notification firebase, change name app

--- android/app/src/main/java/com/reactnativetypescriptboilerplate/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/reactnativetypescriptboilerplate/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.reactnativetypescriptboilerplate.newarchitecture;
+package com.health.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.reactnativetypescriptboilerplate.BuildConfig;
-import com.reactnativetypescriptboilerplate.newarchitecture.components.MainComponentsRegistry;
-import com.reactnativetypescriptboilerplate.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.health.BuildConfig;
+import com.health.newarchitecture.components.MainComponentsRegistry;
+import com.health.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
